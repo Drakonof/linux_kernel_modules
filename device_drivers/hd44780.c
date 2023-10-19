0a910a744bfa2eafd8ec8567bfbabaa1bede21b6
Drakonof <artyom.shimko@gmail.com>
2023-10-19 16:05:56 +0400

    add: created

--- device_drivers/hd44780.c
+++ device_drivers/hd44780.c
@@ -21,63 +21,63 @@ static char data_buf[DARA_BUF_SIZE];
 
 //todo: struct or file
 static unsigned int gpios[] = {
-	3,  // Enable Pin
-	2,  // Register Select Pin
-	4,  // Data Pin 0
-	17, // Data Pin 1
-	27, // Data Pin 2
-	22, // Data Pin 3
-	10, // Data Pin 4
-	9,  // Data Pin 5
-	11, // Data Pin 6
-	5,  // Data Pin 7
+    3,  // Enable Pin
+    2,  // Register Select Pin
+    4,  // Data Pin 0
+    17, // Data Pin 1
+    27, // Data Pin 2
+    22, // Data Pin 3
+    10, // Data Pin 4
+    9,  // Data Pin 5
+    11, // Data Pin 6
+    5,  // Data Pin 7
 };
 
 static void hd44780_enable(void) 
 {
-	gpio_set_value(gpios[0], 1);
-	msleep(5);
-	gpio_set_value(gpios[0], 0);
+    gpio_set_value(gpios[0], 1);
+    msleep(5);
+    gpio_set_value(gpios[0], 0);
 }
 
 static void hd44780_send_byte(char data) 
 {
-	int i =0;
+    int i =0;
 
-	for(i = 0; i < 8; i++) {
-		gpio_set_value(gpios[i + 2], ((data) & (1 << i)) >> i);
-	}
-	hd44780_enable();
-	msleep(5);
+    for(i = 0; i < 8; i++) {
+        gpio_set_value(gpios[i + 2], ((data) & (1 << i)) >> i);
+    }
+    hd44780_enable();
+    msleep(5);
 }
 
 static void hd44780_command(uint8_t data) 
 {
- 	gpio_set_value(gpios[1], 0);	// RS to Instruction
-	hd44780_send_byte(data);
+     gpio_set_value(gpios[1], 0);    // RS to Instruction
+    hd44780_send_byte(data);
 }
 
 static void hd44780_data(uint8_t data) 
 {
- 	gpio_set_value(gpios[1], 1);	/* RS to data */
-	hd44780_send_byte(data);
+     gpio_set_value(gpios[1], 1);    /* RS to data */
+    hd44780_send_byte(data);
 }
 
 static ssize_t module_write(struct file *p_file, const char *p_user_buf, size_t count, loff_t *offs)
 {
-	int to_copy, not_copied, delta, i;
+    int to_copy, not_copied, delta, i;
 
-	to_copy = min(count, sizeof(data_buf));
-	not_copied = copy_from_user(data_buf, p_user_buf, to_copy);
-	delta = to_copy - not_copied;
+    to_copy = min(count, sizeof(data_buf));
+    not_copied = copy_from_user(data_buf, p_user_buf, to_copy);
+    delta = to_copy - not_copied;
 
-	hd44780_command(0x1);
+    hd44780_command(0x1);
 
-	for(i = 0; i < to_copy; i++) {
-		hd44780_data(data_buf[i]);
-	}
+    for(i = 0; i < to_copy; i++) {
+        hd44780_data(data_buf[i]);
+    }
 
-	return delta;
+    return delta;
 }
 
 
@@ -110,7 +110,7 @@ static int __init _module_init(void)
     int i = 0;
 
     //parameter
-	char text[] = "Hello World!";
+    char text[] = "Hello World!";
 
     if (alloc_chrdev_region(&module_nr, 0, 1, _MODULE_NAME) < 0) {
         pr_err(_MODULE_NAME_TO_RP "allocation failed\n");
@@ -152,13 +152,13 @@ static int __init _module_init(void)
     pr_info(_MODULE_NAME_TO_RP "init\n");
 
     // const
-    hd44780_command(0x30);	/* Set the display for 8 bit data interface */
-	hd44780_command(0xf);	/* Turn display on, turn cursor on, set cursor blinking */
-	hd44780_command(0x1);
+    hd44780_command(0x30);    /* Set the display for 8 bit data interface */
+    hd44780_command(0xf);    /* Turn display on, turn cursor on, set cursor blinking */
+    hd44780_command(0x1);
 
-	for(i = 0; i < (sizeof(text) - 1); i++) {
-		hd44780_data(text[i]);
-	}
+    for(i = 0; i < (sizeof(text) - 1); i++) {
+        hd44780_data(text[i]);
+    }
 
     return 0;
 
@@ -181,14 +181,14 @@ static void __exit _module_exit(void)
 {
     int i = 0;
 
-	hd44780_command(0x1);	/* Clear the display */
-	for(i = 0; i < 10; i++){
-		gpio_set_value(gpios[i], 0);
-		gpio_free(gpios[i]);
-	}
-	cdev_del(&module_dev);
-	device_destroy(p_module_class, module_nr);
-	class_destroy(p_module_class);
+    hd44780_command(0x1);    /* Clear the display */
+    for(i = 0; i < 10; i++){
+        gpio_set_value(gpios[i], 0);
+        gpio_free(gpios[i]);
+    }
+    cdev_del(&module_dev);
+    device_destroy(p_module_class, module_nr);
+    class_destroy(p_module_class);
     unregister_chrdev_region(module_nr, 1);
 
     pr_info(_MODULE_NAME_TO_RP "exit\n");
